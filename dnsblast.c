f39de9abf9457b68a1bd16a971bb14d44d0d88b4
John Doe <johndoe@example.com>
2020-06-27 21:06:48 +0100

    t

--- dnsblast.c
+++ dnsblast.c
@@ -372,7 +372,7 @@ main(int argc, char *argv[])
     context.sending = 0;
     while (context.sent_packets != context.received_packets) {
         throttled_receive(&context);
-        printf("Context_",context);
+        printf("%Context",context);
     }
     freeaddrinfo(ai);
     assert(close(sock) == 0);
