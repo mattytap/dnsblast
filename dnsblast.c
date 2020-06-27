2529d060322c794a657529ab797c4013dc7c3c28
John Doe <johndoe@example.com>
2020-06-27 22:45:18 +0100

    t

--- dnsblast.c
+++ dnsblast.c
@@ -157,7 +157,7 @@ get_random_name(char * const name, size_t name_size)
     name[3] = charset_alnum[(r2 >> 16) % sizeof charset_alnum];
     name[4] = '.';    name[5] = 'c';    name[6] = 'o';    name[7] = 'm';
     name[8] = 0;
-printf("%s",name);
+printf("%s \n",name);
     return 0;
 }
 
