d04b8872adc2a3762b8d64f4b4d78c657f36ebd6
John Doe <johndoe@example.com>
2020-06-27 22:59:22 +0100

    t

--- dnsblast.c
+++ dnsblast.c
@@ -71,7 +71,7 @@ encode_name(unsigned char ** const encoded_ptr, size_t encoded_size,
     }
     *encoded++ = 0;
     *encoded_ptr = encoded;
-    printf("*s",encoded_ptr);
+    printf("s",encoded_ptr);
     return 0;
 }
 
