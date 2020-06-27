74c9aa672c631b8ad38257983aeb58a3166a1e74
John Doe <johndoe@example.com>
2020-06-27 17:44:34 +0100

    t

--- dnsblast.c
+++ dnsblast.c
@@ -355,7 +355,7 @@ main(int argc, char *argv[])
         perror("Oops");
         exit(EXIT_FAILURE);
     }
-    printf("%s \n", sock);
+    printf("%d \n", sock);
     init_context(&context, sock, ai, fuzz);
     context.pps = pps;
     srand(0U);
