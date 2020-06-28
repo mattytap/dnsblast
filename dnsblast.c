6c65c2d9bcb13dce5f3a1b52e49857f98c452384
John Doe <johndoe@example.com>
2020-06-28 22:46:15 +0100

    t

--- dnsblast.c
+++ dnsblast.c
@@ -165,7 +165,8 @@ get_random_ptr(char * const name, size_t name_size)
 {
     assert(name_size > (size_t) 8U);
     float paddy = RAND_MAX;
-    const float r1 = rand() / paddy, r2 = 256 * rand() / paddy, r3 = 256 * rand() / paddy, r4 = 256 * rand() / paddy;
+    float r1 = rand() / paddy, r2 = 256 * rand() / paddy, r3 = 256 * rand() / paddy, r4 = 256 * rand() / paddy;
+    assert(r1 = 256 * r1);
     printf("%f %f %f %f\n",r1,r2,r3,r4);
     char hammer[1000];
     sprintf(hammer, "%f", 256 * r1);
