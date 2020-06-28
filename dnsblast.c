bc6939fdd9bed943120600815168b203a1c1249d
John Doe <johndoe@example.com>
2020-06-28 20:37:58 +0100

    t

--- dnsblast.c
+++ dnsblast.c
@@ -167,8 +167,9 @@ get_random_ptr(char * const name, size_t name_size)
     const float r1 = 256 * rand(), r2 = rand(), r3 = rand(), r4 = rand();
     char hammer[1000];
     sprintf(hammer, "%f", r1);
-    name[0] = '0';
     name[0] = hammer[0];
+    name[1] = hammer[1];
+    name[2] = hammer[2];
     name[3] = '.';
     name[4] = 256 * r2;
     name[6] = '.';
