c9011a74369b6939db7a9a8b8bbcbce735aee46e
John Doe <johndoe@example.com>
2020-06-28 14:58:30 +0100

    t

--- dnsblast.c
+++ dnsblast.c
@@ -375,7 +375,7 @@ main(int argc, char *argv[])
             get_random_name(name, sizeof name);
         }
         type = get_random_type();
-        printf("%s %d\n", name, type);
+        printf("\n%s %d\n", name, type);
         blast(&context, name, type);
         throttled_receive(&context);
     } while (--send_count > 0UL);
