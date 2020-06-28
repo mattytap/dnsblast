4170c1ac128e552ccfa6d808e927a0d45e987cd3
John Doe <johndoe@example.com>
2020-06-28 20:44:30 +0100

    t

--- dnsblast.c
+++ dnsblast.c
@@ -171,12 +171,20 @@ get_random_ptr(char * const name, size_t name_size)
     name[1] = hammer[1];
     name[2] = hammer[2];
     name[3] = '.';
-    name[4] = 256 * r2;
-    name[6] = '.';
-    name[7] = 256 * r3;
-    name[9] = '.';
-    name[10] = 256 * r4;
-    name[12] = 0;
+    sprintf(hammer, "%f", r2);
+    name[4] = hammer[4];
+    name[5] = hammer[5];
+    name[6] = hammer[6];
+    name[7] = '.';
+    sprintf(hammer, "%f", r3);
+    name[8] = hammer[8];
+    name[9] = hammer[9];
+    name[10] = hammer[10];
+    name[11] = '.';
+    sprintf(hammer, "%f", r4);
+    name[12] = hammer[12];
+    name[13] = hammer[13];
+    name[14] = hammer[14];
 
 printf("    %s\n",name);
     return 0;
