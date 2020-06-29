7ea58466547b9c5fbfd8d1bce2aecbdd823f18fa
John Doe <johndoe@example.com>
2020-06-29 13:24:37 +0100

    p

--- dnsblast.h
+++ dnsblast.h
@@ -75,10 +75,5 @@ const WeightedType weighted_types[] = {
 #ifndef SO_SNDBUFFORCE
 # define SO_SNDBUFFORCE SO_SNDBUF
 #endif
-    // This program will create different sequence of  
-    // random numbers on every program run  
-  
-    // Use current time as seed for random generator 
-    srand(time(0)); 
 
 #endif
