0e85243495ffe6341458da01ef33b2318f15e247
John Doe <johndoe@example.com>
2020-06-29 12:20:24 +0100

    p

--- dnsblast.c
+++ dnsblast.c
@@ -163,12 +163,12 @@ printf("    %s\n",name);
 static int
 get_random_ptr(char * const name, size_t name_size)
 {
-    srand(time(0)); 
     assert(name_size > (size_t) 15U);
-    int octet1 = (rand() % 256); 
-    int octet2 = (rand() % 256); 
-    int octet3 = (rand() % 256);  
-    int octet4 = (rand() % 256);  
+    int lower = 0, upper = 255; 
+    int octet1 = (rand() % (upper - lower + 1)) + lower; 
+    int octet2 = (rand() % (upper - lower + 1)) + lower; 
+    int octet3 = (rand() % (upper - lower + 1)) + lower; 
+    int octet4 = (rand() % (upper - lower + 1)) + lower; 
     sprintf(name, "%d%d%d%d" ,octet1,octet2,octet3,octet4);
     printf("%s%s ", ".",name); 
     printf("    %s\n",name);
