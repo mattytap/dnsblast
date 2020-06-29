1c347da781d1feaf94f67c452a30d752e0e780a9
John Doe <johndoe@example.com>
2020-06-29 21:46:36 +0100

    p

--- dnsblast.c
+++ dnsblast.c
@@ -230,7 +230,7 @@ receive(Context * const context)
     unsigned char buf[MAX_UDP_DATA_SIZE];
 
     while (recv(context->sock, buf, sizeof buf, 0) == (ssize_t) -1) {
-     printf("\n<----- %d %d \n",context->id,context->sock);
+     printf("\n<----- %d %d \n",context->id,context->ai->ai_socktype);
        if (errno == EAGAIN) {
             return 1;
         }
