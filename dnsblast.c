e0ac85d71d2a914891aed4e7a86e12b6f149c04d
John Doe <johndoe@example.com>
2020-06-29 19:30:07 +0100

    i

--- dnsblast.c
+++ dnsblast.c
@@ -287,7 +287,7 @@ empty_receive_queue(Context * const context)
 static int
 throttled_receive(Context * const context)
 {
-    printf(" %ld ",context->id);
+    printf(" %d ",context->id);
     unsigned long long       now = get_nanoseconds(), now2;
     const unsigned long long elapsed = now - context->startup_date;
     const unsigned long long max_packets =
