37f4b8d997bb187fdebcbebe5c38316344d00976
John Doe <johndoe@example.com>
2020-06-30 15:32:10 +0100

    P

--- dnsblast.c
+++ dnsblast.c
@@ -138,7 +138,7 @@ resolve(const char * const host, const char * const port)
         .ai_protocol = IPPROTO_UDP
     };
     const int gai_err = getaddrinfo(host, port, &hints, &ai);
-    printf("\n141 HOST:%s PORT:%s SA_DATA:%s\n",host,port,ai->ai_addr->sa_data);
+    printf("141 HOST:%s PORT:%s SA_DATA:%s\n",host,port,ai->ai_addr->sa_data);
 
     if (gai_err != 0) {
         fprintf(stderr, "[%s:%s]: [%s]\n", host, port, gai_strerror(gai_err));
@@ -223,7 +223,7 @@ get_sock(const char * const host, const char * const port,
     setsockopt(sock, IPPROTO_IP, IP_DONTFRAG, &(int[]) { 0 }, sizeof (int));
 #endif
     assert(ioctl(sock, FIONBIO, &flag) == 0);
-printf("%d\n",sock);
+printf("226 %d\n",sock);
     return sock;
 }
 
