3f974db6e8ef61886317e4c279b5b919045843cc
John Doe <johndoe@example.com>
2020-07-01 17:23:37 +0100

    p

--- dnsblast.c
+++ dnsblast.c
@@ -235,15 +235,18 @@ receive(Context * const context)
     unsigned char buf[MAX_UDP_DATA_SIZE];
 
     while (recv(context->sock, buf, sizeof buf, 0) == (ssize_t) -1) {
-    printf("        R238 <-----    ID:%d SI_DATA:%s SI_FAMILY:%d AI_ADDRLEN:%d \n",context->id,context->ai->ai_addr->sa_data,context->ai->ai_addr->sa_family,context->ai->ai_addrlen);
+    printf("        R238 <-----    ID:%d SOCK:%s SENDING:%d SENT_PACKETS:%d RECEIVED_PACKETS:%ld ADDRLEN:%ld BUF:%d\n",context->id,context->sock,context->sending,context->sent_packets,context->received_packets,context->ai->ai_addrlen,buf)
+    
+    
+    context->ai->ai_addr->sa_data,context->ai->ai_addr->sa_family,context->ai->ai_addrlen);
        if (errno == EAGAIN) {
             return 1;
         }
         assert(errno == EINTR);
     }
-    printf("        R242 <-----    ID:%d SI_DATA:%s SI_FAMILY:%d AI_ADDRLEN:%d \n",context->id,context->ai->ai_addr->sa_data,context->ai->ai_addr->sa_family,context->ai->ai_addrlen);
+    printf("        R244 <-----    ID:%d SI_DATA:%s SI_FAMILY:%d AI_ADDRLEN:%d \n",context->id,context->ai->ai_addr->sa_data,context->ai->ai_addr->sa_family,context->ai->ai_addrlen);
     context->received_packets++;
-    printf("        R244 <-----    ID:%d RECEIVED_PACKET:%ld SA_DATA:%s\n",context->id,context->received_packets,context->ai->ai_addr->sa_data);
+    printf("        R246 <-----    ID:%d RECEIVED_PACKET:%ld SA_DATA:%s\n",context->id,context->received_packets,context->ai->ai_addr->sa_data);
 
     return 0;
 }
