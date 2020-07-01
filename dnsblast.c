4e8c2a9d792d0fa334e0173da7c0680c38ea677f
John Doe <johndoe@example.com>
2020-07-01 06:22:09 +0100

    p

--- dnsblast.c
+++ dnsblast.c
@@ -378,7 +378,7 @@ main(int argc, char *argv[])
         perror("Oops");
         exit(EXIT_FAILURE);
     }
-    printf("A381 HOST:%s PORT:%s SOCK:%d %s\n",host,port,sock,sockaddr_in);
+    printf("A381 HOST:%s PORT:%s SOCK:%d %s\n",host,port,sock,ai->ai_addr->sa_data,ai->ai_addrlen);
     init_context(&context, sock, ai, fuzz);
     context.pps = pps;
     srand(clock()); //fixes problem with lack of randomness of rand(). MF 20200629
