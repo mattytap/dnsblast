e3b20f768bc46b4b7de3bf12a1a0f660e744026b
John Doe <johndoe@example.com>
2020-07-01 14:03:47 +0100

    i

--- dnsblast.c
+++ dnsblast.c
@@ -235,15 +235,15 @@ receive(Context * const context)
     unsigned char buf[MAX_UDP_DATA_SIZE];
 
     while (recv(context->sock, buf, sizeof buf, 0) == (ssize_t) -1) {
-    printf("      R238 <-----      ID:%d SI_DATA:%s SI_FAMILY:%d AI_ADDRLEN:%d \n",context->id,context->ai->ai_addr->sa_data,context->ai->ai_addr->sa_family,context->ai->ai_addrlen);
+    printf("        R238 <-----    ID:%d SI_DATA:%s SI_FAMILY:%d AI_ADDRLEN:%d \n",context->id,context->ai->ai_addr->sa_data,context->ai->ai_addr->sa_family,context->ai->ai_addrlen);
        if (errno == EAGAIN) {
             return 1;
         }
         assert(errno == EINTR);
     }
-    printf("      R242 <-----      ID:%d SI_DATA:%s SI_FAMILY:%d AI_ADDRLEN:%d \n",context->id,context->ai->ai_addr->sa_data,context->ai->ai_addr->sa_family,context->ai->ai_addrlen);
+    printf("        R242 <-----    ID:%d SI_DATA:%s SI_FAMILY:%d AI_ADDRLEN:%d \n",context->id,context->ai->ai_addr->sa_data,context->ai->ai_addr->sa_family,context->ai->ai_addrlen);
     context->received_packets++;
-    printf("      R244 <-----      ID:%d RECEIVED_PACKET:%ld SA_DATA:%s\n",context->id,context->received_packets,context->ai->ai_addr->sa_data);
+    printf("        R244 <-----    ID:%d RECEIVED_PACKET:%ld SA_DATA:%s\n",context->id,context->received_packets,context->ai->ai_addr->sa_data);
 
     return 0;
 }
@@ -285,10 +285,10 @@ periodically_update_status(Context * const context)
 static int
 empty_receive_queue(Context * const context)
 {
-    printf("   ER288 <-----        ID:%d RECEIVED_PACKETS:%ld SENT_PACKETS:%ld\n", context->id,context->received_packets,context->sent_packets);
+    printf("     ER288 <-----      ID:%d RECEIVED_PACKETS:%ld SENT_PACKETS:%ld\n", context->id,context->received_packets,context->sent_packets);
     while (receive(context) == 0)
         ;
-    printf("   ER291 <-----        ID:%d RECEIVED_PACKETS:%ld SENT_PACKETS:%ld\n", context->id,context->received_packets,context->sent_packets);
+    printf("     ER291 <-----      ID:%d RECEIVED_PACKETS:%ld SENT_PACKETS:%ld\n", context->id,context->received_packets,context->sent_packets);
     periodically_update_status(context);
 
     return 0;
