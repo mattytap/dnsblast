f1217ac6360d728b12f607d5d5875bcb3fa8fc17
John Doe <johndoe@example.com>
2020-08-30 22:44:13 +0100

    sun4

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,6 +1,6 @@
 // Client side implementation of UDP client-server model
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 //#include <sys/types.h>
@@ -8,7 +8,7 @@
 //#include <arpa/inet.h>
 #include <netinet/in.h>
 #define V4IP 0x0A000004
-#define PORT 53
+#define PORT 5335
 #define MAXLINE 1024
 
 int main()
@@ -18,14 +18,24 @@ int main()
     const char *hello = "Hello from client";
     struct sockaddr_in servaddr;
     // Creating socket file descriptor
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    memset(&servaddr, 0, sizeof(servaddr));
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    memset(&servaddr, 0, sizeof(servaddr));  // zap server information
     // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_family = AF_INET; // IP protocol family.
+    servaddr.sin_port = htons(PORT); // 53
+    servaddr.sin_addr.s_addr = INADDR_ANY; // (in_addr_t) 0x00000000
+    servaddr.sin_addr.s_addr = 0x0A000004;
     int n, len;
-    sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    size_t sendtov;
+    if ((sendtov = sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != strlen(hello))
+    {
+        perror("sendto failed");
+        exit(EXIT_FAILURE);
+    }
     printf("Hello message sent.\n");
     n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
     buffer[n] = '\0';
