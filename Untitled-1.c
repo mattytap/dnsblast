ac1ee4e31d83c8be8a88e02c456dd3d353bc4456
John Doe <johndoe@example.com>
2020-08-31 05:27:34 +0100

    mon1

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,45 +1,59 @@
+
 // Client side implementation of UDP client-server model
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-//#include <sys/types.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
-//#include <arpa/inet.h>
+#include <arpa/inet.h> 
 #include <netinet/in.h>
-#define V4IP 0x0A000004
-#define PORT 5335
+
+#define HOST    ((const unsigned char *)"10.0.0.4")
+#define PORT    5336
 #define MAXLINE 1024
 
-int main()
-{
+// Driver code 
+int main() { 
     int sockfd;
     char buffer[MAXLINE];
-    const char *hello = "Hello from client";
-    struct sockaddr_in servaddr;
+    char *hello = "Hello from client"; 
+    struct sockaddr_in     servaddr; 
+  
     // Creating socket file descriptor
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
+    { 
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
-    memset(&servaddr, 0, sizeof(servaddr));  // zap server information
+
+    // zap server information
+    memset(&servaddr, 0, sizeof(servaddr));
+
     // Filling server information
     servaddr.sin_family = AF_INET; // IP protocol family.
-    servaddr.sin_port = htons(PORT); // 53
-    servaddr.sin_addr.s_addr = INADDR_ANY; // (in_addr_t) 0x00000000
-    servaddr.sin_addr.s_addr = 0x0A000004;
-    int n, len;
-    size_t sendtov;
-    if ((sendtov = sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != strlen(hello))
+    servaddr.sin_port = htons(PORT); // 53 Big Endian short interger
+    servaddr.sin_addr.s_addr = inet_addr(HOST); // Big Endian format. see htonl()
+    const char *matt = inet_ntop(AF_INET,0,&servaddr.sin_addr.s_addr,strlen(&servaddr.sin_addr.s_addr));
+
+    printf("Matt: %s\n", matt);
+    int len;
+    size_t sendtov, n;
+    if ((sendtov = sendto(sockfd, (const char *)hello, strlen(hello), 
+        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+            sizeof(servaddr))) != strlen(hello))
     {
         perror("sendto failed");
         exit(EXIT_FAILURE);
     }
     printf("Hello message sent.\n");
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
+
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+        MSG_WAITFORONE, (struct sockaddr *) &servaddr, 
+                &len);
     buffer[n] = '\0';
     printf("Server : %s\n", buffer);
+
     close(sockfd);
     return 0;
 }
